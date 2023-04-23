e9e1ae3db1a4ae91dcc692dc1bd320f127d3b140
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-23 16:02:34 +0530

    login System start working

--- static/js/main.js
+++ static/js/main.js
@@ -6,14 +6,28 @@ let userLoginEmailAndName = document.getElementById("userLoginEmail");
 let userLoginPassword = document.getElementById('userLoginPassword');
 let stayLoginCheckBox = document.getElementById('stayLoginCheckBox');
 
-loginBtn.addEventListener('click', (e)=>{
+loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const gettingUserDetailsObj = JSON.parse(localStorage.getItem('userDetails'))
-    if(userLoginEmailAndName.value == gettingUserDetailsObj.userEmail || userLoginEmailAndName.value == gettingUserDetailsObj.userName){
-        console.log("This is user is already loged in our website...");
+    let alertsDiv = document.getElementById('alerts');
+    if ((userLoginEmailAndName.value === gettingUserDetailsObj.userEmail || userLoginEmailAndName.value === gettingUserDetailsObj.userName) && (userLoginPassword.value === gettingUserDetailsObj.userPassword)) {
+        console.log("you CAN Login");
         console.log(`${userLoginEmailAndName.value} == ${gettingUserDetailsObj.userEmail}\n${userLoginEmailAndName.value}==${gettingUserDetailsObj.userName}`)
-    }else{
-        console.log("You can login...")
+        alertsDiv.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Hurrey!!</strong> Login Successfully
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`
+    }
+    else {
+        console.error("You CAN'T login..., Incurrect User Name and Incurrect Password")
+        alertsDiv.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Oops!!</strong> Incurrect User Name or Incurrect Password
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`
     }
 })
 
@@ -23,7 +37,7 @@ let userCreateAccountEmail = document.getElementById('userCreateAccountEmail')
 let userCreateAccountPassword = document.getElementById('userCreateAccountPassword')
 let saveUserDetailsCheck = document.getElementById('saveDetailCheck')
 let userCreateAccountBtn = document.getElementById('userCreateAccountBtn');
-userCreateAccountBtn.addEventListener('click', (e)=>{
+userCreateAccountBtn.addEventListener('click', (e) => {
     e.preventDefault()
     // let arrOfUsers = []
     // let userDetailsObj = new Object();
@@ -41,16 +55,21 @@ userCreateAccountBtn.addEventListener('click', (e)=>{
     // }
 })
 
-const emailVerification = (emailInText)=>{
+const emailVerification = (emailInText) => {
     //rajnirala910@gmail.com
-    if(emailInText.isUpper()){
-        if(emailInText.length >= 10){
-            let symbolsArr = ["!","#","%","$","^","&","*","(",")","-","+","=", "/", ",","|","{","}","[","]", "`","~", "?", "<", ">", ":", ";"];
-            for(EmailSymbol of symbolsArr){
-                if(emailInText.includes(EmailSymbol)){
-                    
-                }
+    if (!(emailInText.isUpper())) {
+        if (emailInText.length >= 10) {
+            let symbolsArr = ["!", "#", "%", "$", "^", "&", "*", "(", ")", "-", "+", "=", "/", ",", "|", "{", "}", "[", "]", "`", "~", "?", "<", ">", ":", ";"];
+            const symbolIsAvailible = symbolsArr.filter(sm => emailInText.includes(sm))
+            if (symbolIsAvailible) {
+                console.log(`symbols are not aloud`)
             }
+        } else {
+            console.log(`Email is too short, try again`);
         }
+    } else {
+        console.log(`Capital char is note aloud.. try again`);
     }
 }
+
+
