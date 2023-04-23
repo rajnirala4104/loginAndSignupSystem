d0cc9e4ae1b5ee4f65319d4fd4505cd0c63ba70e
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-23 17:35:25 +0530

    read data from db

--- database/db.js
+++ database/db.js
@@ -1,1 +1,38 @@
 const { MongoClient } = require('mongodb')
+
+
+
+const uri = "mongodb+srv://rajmongo:rajmongo@cluster0.2yskbyq.mongodb.net/?retryWrites=true&w=majority";
+const myClient =  new MongoClient(uri)
+async function main(){
+    try{
+        await myClient.connect()
+        console.log(`database connect successfully`)
+        await readDataFromDatabase(myClient)
+        // await showingAllTheDatabases(myClient)
+    }catch(e){
+        console.log("Oops!! I'm facing porblem to connect the database", e)
+    }finally{
+        await myClient.close()
+    }
+}
+
+async function showingAllTheDatabases(client) {
+    const dbs = await client.db().admin().listDatabases();
+    dbs.databases.forEach(db => {
+        console.log(`- ${db.name}`)
+    });
+}
+
+
+
+async function readDataFromDatabase(Client) {
+    const dbData = await Client.db('logingAndRegisterSystem').collection('userDetails').find({}).toArray()
+    if(dbData.length > 0){
+        console.log(dbData)
+    }else{
+        console.log("Oops!! there is nothing to show here..")
+    }
+}
+
+main()
