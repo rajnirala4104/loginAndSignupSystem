ba0466e68229a2e9fcb6d65474fd8ba968024001
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-29 16:20:38 +0530

    hbs added

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const path = require('path')
-const expressLayouts = require('express-ejs-layouts')
+const hbs = require('hbs')
 const { main, readDataFromDatabase, myClient, insertDataInDatabase} = require('./database/db')
 
 main()
 const app = express()
 app.use(express.urlencoded({ extended: false }));
-app.use(expressLayouts)
-app.set('view engine', 'ejs');
+// app.use(expressLayouts)
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(__dirname + '/static'));
 
+
 app.get('/', (request, response) => {
-    readDataFromDatabase(myClient).then(result => result, err=>err)
-    response.status(200).render('index')
+    const data = readDataFromDatabase(myClient).then(result => result, err=>err)
+    response.status(200).render('layout', {myData: data})
 })
 
-app.post('/accountcreatedone', (request, response)=>{
 
+app.post('/accountcreatedone', (request, response)=>{
     console.log(request.body)
     insertDataInDatabase(myClient, request.body).then(result => result, err => err)
-    response.status(200).render('index')
+    response.status(200).render('createAccount', {userData: request.body})
 })
 
 
