74ed005a7d309706a93bbc421b1ba4f950278fc8
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-05-01 03:04:58 +0530

    litbit changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const { main, readDataFromDatabase, myClient, insertDataInDatabase, deleteTheDataOfTheDatabase, updateDataOfDatabase, deleteTheMultipalDataOfTheDatabase } = require('./database/db')
+const { resolve6 } = require('dns')
 
 main()
 const app = express()
@@ -12,43 +13,48 @@ app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(__dirname + '/static'));
 
 
-app.get('/', (request, response) => {
-    readDataFromDatabase(myClient).then(result => console.log(result), err => err)
-    response.status(200).render('index')
-})
+try{
+    app.get('/', (request, response) => {
+        readDataFromDatabase(myClient).then(result => console.log(result), err => err)
+        response.status(200).render('index')
+    })
 
-
-
-app.post('/accountcreatedone', (request, response) => {
-    const data = request.body
-    var _datafound = false;
-    readDataFromDatabase(myClient).then(databaseData => {
-        databaseData.forEach(singleData => {
-            if (data.userName === singleData.userName && data.userEmail === singleData.userEmail) {
-                _datafound = true;
-            }
-        })
-
-        if (_datafound === false) {
-            insertDataInDatabase(myClient, data).then(result => result, err => err)
-            response.status(200).render('createAccount.hbs', {
-                DublicateData_NOT_FoundError: true,
-                message : "Data Inserted Successfully",
-                UserDetails: {
-                    userName: data.userName,    
-                    userEmail: data.userEmail
+    app.post('/accountcreatedone', (request, response) => {
+        const data = request.body
+        var _datafound = false;
+        readDataFromDatabase(myClient).then(databaseData => {
+            databaseData.forEach(singleData => {
+                if (data.userName === singleData.userName && data.userEmail === singleData.userEmail) {
+                    _datafound = true;
                 }
             })
-        }else{
-            response.status(200).render('index', { 
-                DublicateDataFoundError: _datafound,
-                message : "This data is already we have"
-            })
-        }
+
+            if (_datafound === false) {
+                insertDataInDatabase(myClient, data).then(result => result, err => err)
+                response.status(200).render('createAccount.hbs', {
+                    DublicateData_NOT_FoundError: true,
+                    message : "Data Inserted Successfully",
+                    UserDetails: {
+                        userName: data.userName,    
+                        userEmail: data.userEmail
+                    }
+                })
+            }else{
+                response.status(200).render('index', { 
+                    DublicateDataFoundError: _datafound,
+                    message : "This data is already we have"
+                })
+            }
+        })
     })
-})
 
-
-
+    app.post('/logindone', (req, res)=>{
+        res.status(200).render('loginDone', {MyName:"Raj Nirala"});
+    })
+}catch(e){
+    app.get("/", (req, res)=>{
+        res.status(404).render('404Error', {error: e})
+    })
+}
 
 module.exports = app;
